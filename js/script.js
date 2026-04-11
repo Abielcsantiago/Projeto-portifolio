96986eba1a9d2b8df813605d004479e5236f8236
Abiel <abielcsantiago@gmail.com>
2026-04-10 22:41:20 -0300

    Melhoria no script do menu mobile

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const mobileBttn = document.querySelector("#mobile-bttn");
 const nav = document.querySelector("header nav");
+const navLinks = document.querySelectorAll(".nav-links")
 mobileBttn.addEventListener('click',(e) => {
     nav.classList.toggle("mobile")
     mobileBttn.classList.toggle("ativo")
 });
+navLinks.forEach((e) => {
+    e.addEventListener('click', () =>{
+        nav.classList.remove('mobile')
+        mobileBttn.classList.remove("ativo")
+    })
+})
