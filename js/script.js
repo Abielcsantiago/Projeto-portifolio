cf86db070780b60df95367d7a3cfb196b1c1bbee
Abiel <abielcsantiago@gmail.com>
2026-05-24 11:11:21 -0300

    Atualizando backtotop e estilos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const mobileBttn = document.querySelector("#mobile-bttn");
 const nav = document.querySelector("header nav");
 const navLinks = document.querySelectorAll(".nav-links")
+const backToTop = document.querySelector("#bttn-back-top")
 mobileBttn.addEventListener('click',(e) => {
     nav.classList.toggle("mobile")
     mobileBttn.classList.toggle("ativo")
@@ -9,5 +10,13 @@ navLinks.forEach((e) => {
     e.addEventListener('click', () =>{
         nav.classList.remove('mobile')
         mobileBttn.classList.remove("ativo")
+    })
+})
+backToTop.addEventListener("click", e =>{
+    e.preventDefault()
+    scrollTo({
+        top:0,
+        left:0,
+        behavior:"smooth"
     })
 })
